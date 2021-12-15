a65d549cb3029c93551890234d09bab73076be8d
Adriano Perlini <aperlini@protonmail.ch>
2021-12-15 20:41:41 +0100

    default bv

--- filename/rename.sh
+++ filename/rename.sh
@@ -4,35 +4,46 @@ FOLDER="$1"
 
 if [[ $# -lt 1 ]]; then
    echo "missing foldername arg"
-fi
-
-if [[ -d $FOLDER ]]; then
-   parse $FOLDER
 else
-   echo "$FOLDER is not a folder."
+   if [[ -d $FOLDER ]]; then
+      parse $FOLDER
+   else
+      echo "$FOLDER is not a folder."
+   fi
 fi
 
 function file_extension {
    if [[ -f $1 ]]; then
       case "$1" in
-         *.png) echo "$1 is a PNG file" ;;
-         *.jpg) echo "$1 is a JPG file" ;;
+         *.png) echo '.png' ;;
+         *.jpg) echo '.jpg' ;;
          *) echo "$1 is not JPG or PNG file" ;;
       esac
    else
       echo "$1 : file extension not valid"
    fi
+}
 
+function save {
+   origin=$1
+   rename="$2/$3$4"
+   /bin/mv $origin $rename
 }
 
 function parse {
+   i=0
    for FILE in $1/*; do
-      if [[ -f $FILE ]]; then
-         file_extension $FILE
+      if [[ -f $FILE ]]; then 
+         EXT="$(file_extension $FILE)"
+         i=$((i+1))
+         EXTENT=${FILE##*.} # echo 'png'
+         PATH=${FILE%/*} 
+         save $FILE $PATH $i $EXT
       else
          echo "$FILE is not valid"
       fi
    done
+   echo "$i file(s) successfully renamed"
 }
 
 
