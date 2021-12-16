e9605abdcbda75bcaaf63b3b774fa0aa84e3df49
Adriano Perlini <aperlini@protonmail.ch>
2021-12-16 22:01:46 +0100

    pre rule

--- filename/rename.sh
+++ filename/rename.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 FOLDER="$1"
+PRE="$2"
 
 if [[ $# -lt 1 ]]; then
-   echo "missing foldername arg"
+   echo "usage : rename.sh folder [prepend]"
 else
    if [[ -d $FOLDER ]]; then
-      parse $FOLDER
+      parse $FOLDER $PRE
    else
       echo "$FOLDER is not a folder."
    fi
@@ -15,8 +16,8 @@ fi
 function file_extension {
    if [[ -f $1 ]]; then
       case "$1" in
-         *.png) echo '.png' ;;
-         *.jpg) echo '.jpg' ;;
+         *.png) echo 'png' ;;
+         *.jpg) echo 'jpg' ;;
          *) echo "$1 is not JPG or PNG file" ;;
       esac
    else
@@ -26,18 +27,23 @@ function file_extension {
 
 function save {
    origin=$1
-   dest="$2/$3$4"
+   dest="$2/$3"
    /bin/mv $origin $dest
 }
 
 function parse {
-   i=0
+   INDEX=0
+   FORMAT=$2
    for FILE in $1/*; do
       if [[ -f $FILE ]]; then 
          EXT="$(file_extension $FILE)"
-         i=$((i+1))
+         INDEX=$((INDEX+1))
          PATH=${FILE%/*} 
-         save $FILE $PATH $i $EXT
+         F="$INDEX.$EXT"
+         if [[ -n $FORMAT ]]; then
+            F="$PRE-$INDEX.$EXT"
+         fi
+         save $FILE $PATH $F
       else
          echo "$FILE is not valid"
       fi
