b25d8e4fd646cc66560cf3762682aaa3f17b1c7e
adriano.perlini <adriano.perlini@etu.hesge.ch>
2023-11-22 13:26:31 +0100

    improvements

--- weblock-to-md/link2md.sh
+++ weblock-to-md/link2md.sh
@@ -3,11 +3,24 @@
 set -e
 
 folder="exports"
-value=$(cat * | grep -oP '(?<=<string>).*?(?=</string>)')
+
 if [[ ! -d "$folder" ]]; then
 	mkdir $folder
 fi
 
-for v in $value; do
-   echo "[$v]($v)" >> $folder/links.md
+echo "fils de pute"
+
+for entry in *; do
+    echo "$entry"
+    if [[ ! -d "$entry" ]] && [[ "${entry#*.}" == "webloc" ]]; then
+        link=$(cat "$entry" | grep -oP '(?<=<string>).*?(?=</string>)')
+        title=$(basename -s '.webloc' "$entry") 
+        echo "[$title]($link)" >> $folder/links.md
+        rm "$entry"
+    fi
 done
+
+echo "========================================================"
+echo "all links where extracted and saved => ./export/links.md"
+echo "========================================================"
+
